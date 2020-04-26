97cee939ef1de31de873f4acfef2068057935261
Jared  E <jared.epps322@gmail.com>
2020-04-26 19:37:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,8 +30,8 @@ end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
    holiday_supplies.each do |season, data|
-#     puts "#{season.to_s.capitalize!}:"
-#     data.each do |holiday, supply|
+    puts "#{season.to_s.capitalize!}:"
+    data.each do |holiday, supply|
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
