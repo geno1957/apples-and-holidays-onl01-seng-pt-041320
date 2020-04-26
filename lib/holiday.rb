b81b6c3ebb36e2a5c340feff4580e21730f3e3db
Jared  E <jared.epps322@gmail.com>
2020-04-26 19:53:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,13 +57,7 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.map do |season, holiday|
-    holiday.map do |holiday, supply|
-      holiday if supply.include?("BBQ")
-    end
-  flatten.compact
-  end
-  end
+ 
 
 
 
